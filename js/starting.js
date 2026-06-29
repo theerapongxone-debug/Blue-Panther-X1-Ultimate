62f2d0d7349898e0479cb3427b3d505067a9176e
theerapongxone-debug <theerapongxone@gmail.com>
2026-06-30 01:30:29 +0700

    Implement countdown timer in starting.js

--- js/starting.js
+++ js/starting.js
@@ -1,1 +1,29 @@
+let total=300;
 
+function update(){
+
+const min=Math.floor(total/60);
+
+const sec=total%60;
+
+document.getElementById("countdown").textContent=
+
+String(min).padStart(2,"0")
+
++":"
+
++
+
+String(sec).padStart(2,"0");
+
+if(total>0){
+
+total--;
+
+}
+
+}
+
+update();
+
+setInterval(update,1000);
